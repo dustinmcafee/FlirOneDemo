aad75b6d3306c198898aae47e94563872a4b2c3f
Dustin McAfee <dustin.mcafee@elotouch.com>
2020-04-08 23:21:13 -0400

    Added Application to Manifest

--- app/src/main/java/com/samples/flironecamera/FlirCameraApplication.java
+++ app/src/main/java/com/samples/flironecamera/FlirCameraApplication.java
@@ -2,12 +2,9 @@ package com.samples.flironecamera;
 
 import android.app.Application;
 
-import com.flir.thermalsdk.androidsdk.live.connectivity.UsbPermissionHandler;
 import com.flir.thermalsdk.live.Identity;
 
-import java.util.concurrent.LinkedBlockingQueue;
-
-public class FlirCameraContext extends Application {
+public class FlirCameraApplication extends Application {
     public static CameraHandler cameraHandler;
     public static Identity connectedIdentity;
 }
